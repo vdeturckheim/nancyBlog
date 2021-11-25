5368b987fc080e5526407abf24c4639bb0a5fc5b
vdeturckheim <vlad2t@hotmail.com>
2021-11-25 11:25:25 +0100

    add tests

--- test/test.js
+++ test/test.js
@@ -4,5 +4,9 @@ describe('Array', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
+
+        it('should return the index of an element', function() {
+            assert.equal([1, 2, 3].indexOf(3), 2);
+        });
     });
 });
