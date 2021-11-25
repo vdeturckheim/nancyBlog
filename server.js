a60cd47d783f0931e4159807c844254057237423
vdeturckheim <vlad2t@hotmail.com>
2021-11-25 10:27:11 +0100

    add code

---
+++ server.js
@@ -0,0 +1,26 @@
+const Express = require('express');
+const app = Express();
+const DB = require('./database/db.js');
+
+app.get('/posts/:id', (req, res) => {
+    const postId = req.params.id;
+    DB.all('SELECT * FROM posts WHERE ID = ?', [postId], (err, rows) => {
+        if (err) {
+            console.log(err);
+            res.end('error');
+        }
+        res.json(rows);
+    });
+})
+
+app.get('/posts', (req, res) => {
+    DB.all('SELECT ID, TITLE FROM posts', (err, rows) => {
+        if (err) {
+            console.log(err);
+            res.end('error');
+        }
+        res.json(rows);
+    });
+});
+
+app.listen(8080);
